5d79a623c7c0c68939ce505338be0881834a8f2e
Pratik Chandra <maxpatrick001@gmail.com>
2019-10-16 19:09:55 +0530

    added positive negative file

---
+++ PratikChandra/positiveNegative.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int a,p=0,n=0;
+	printf("Enter the set of numbers\n");
+	while(a!=0){
+		scanf("%d",&a);
+	if(a<0)
+		n++;
+	else if(a>0)
+		p++;
+	}
+	printf("The number of positive numbers %d and number of negative numbers %d \n",p,n);
+	return 0;
+}
