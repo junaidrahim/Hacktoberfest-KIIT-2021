7c1688c0f367e0a071aea28dcfa6d4cb33af0675
Anurag Singh <55041899+anurag03-10@users.noreply.github.com>
2019-10-25 02:12:23 +0530

    Update sum_of_digits.c

--- Amrit Ranjan/sum_of_digits.c
+++ Amrit Ranjan/sum_of_digits.c
@@ -5,10 +5,11 @@ void main()
     int n,sum=0,m;
     printf("Enter Number : ");
     scanf("%d",&n);
+    l=n;
     for(int i=0; i<8; i++)
     {
         sum=sum+(n%10);
         n=n/10;
     }
-        printf("Sum of digits : %d\n",sum);
+        printf("Sum of digits of %d is : %d\n",n,sum);
 }
