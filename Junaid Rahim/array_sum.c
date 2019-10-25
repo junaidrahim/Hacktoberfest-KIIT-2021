492d00d57d74d97723570d3c92b6fb822af7f8df
sanktiwari <56881128+sanktiwari@users.noreply.github.com>
2019-10-25 21:42:11 +0530

    Update array_sum.c

--- Junaid Rahim/array_sum.c
+++ Junaid Rahim/array_sum.c
@@ -2,29 +2,33 @@
 
 #include <stdio.h>
 
-int sum(int size, int arr[size]){
+int sum(int size, int arr[size])
+   {
     int arr_sum = 0;
 
     if(size == 1)
         return arr[0];
     
-    else {
+    else
+    {
         arr_sum = arr[size-1] +  sum(--size, arr);
         return arr_sum;
     }   
 }
 
-int main(){
+int main()
+   {
     int n;
-    printf("Enter Size of Array: ");
+    printf("Enter Size of Array is: ");
     scanf("%d", &n);
 
     int arr[n];
     printf("Enter elements of the array: ");
 
-    for(int i=0; i<n; i++){
+    for(int i=0; i<n; i++)
+    {
         scanf("%d", &arr[i]);
     }
     printf("The sum of the array is %d\n", sum(n, arr));
-    return 0;
-}
+      return 0;
+   }
