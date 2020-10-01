0d206bb299842e017713a417da164e666c516710
Raphael O <raphael.ob@protonmail.com>
2020-10-01 10:30:46 +0200

    Bubble Sort : Simple array sorting algorithm

---
+++ Raphael/BubbleSort.cs
@@ -0,0 +1,49 @@
+/*
+ * Author: Lockna
+ */
+
+using System;
+
+namespace BubbleSort {
+    class Bubble {
+
+        static void Main(string[] args) 
+        {
+            int[] arr = { 78, 55, 45, 98, 13};
+            int temp;
+
+            Console.WriteLine("Bubble Sort");
+            Console.Write("Initial array is: ");
+
+
+            // Prints the unchanged array
+            foreach (int p in arr)
+              Console.Write(p + " ")
+
+            for (int j = 0; j <= arr.Length - 2; j++) 
+            {
+
+		            for (int i = 0; i <= arr.Length - 2; i++) 
+		            {
+                    if (arr[i] > arr[i + 1]) 
+		                {
+                        // If a number is bigger than the successor they get swaped.
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+
+        Console.WriteLine("Sorted:");
+
+        // Prints the sorted key
+        foreach (int p in arr)
+            Console.Write(p + " ");
+
+        // Holds the console window on windows open, since it closes immediately after program end.
+        Console.ReadKey();
+
+      }
+   }
+}
