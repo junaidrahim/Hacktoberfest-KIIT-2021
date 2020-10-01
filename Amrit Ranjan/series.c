e70fc827f080633190b8e343d8a2ed6cf2833109
Programmer <55909772+YashMirge@users.noreply.github.com>
2020-10-01 19:33:17 +0530

    Updated

--- Amrit Ranjan/series.c
+++ Amrit Ranjan/series.c
@@ -2,10 +2,10 @@
 
 void main()
 {   
-    int n,m=1,last,t=0;
+    int n,i,m=1,last,t=0;
     printf("Enter End : ");
     scanf("%d",&n);
-    for(int i=0; i<n; i++)
+    for(i=0; i<n; i++)
     {
         printf("%d ",m);
     	last=t;
