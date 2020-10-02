b115657a7d22a948028bd081e6b628a24f0043a4
gauravahya <56601795+gauravahya@users.noreply.github.com>
2020-10-02 12:14:31 +0530

    Update reverse_array.c (#4)

--- Amrit Ranjan/reverse_array.c
+++ Amrit Ranjan/reverse_array.c
@@ -5,7 +5,7 @@ void main()
 	int n,i,arr[100];
 	printf("Enter the number of elements you want the array upto : ");
 	scanf("%d",&n);
-	printf("Enter the elements of the array :\n");
+	printf("Enter the elements of the array :\n\n");
 	for(i=0;i<n;i++)
 	{
 		scanf("%d",&arr[i]);
