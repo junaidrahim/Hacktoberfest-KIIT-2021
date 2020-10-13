4f73eba274a495dd921265225fbe5578efc963a0
Atig Purohit <itsthechamp0074@gmail.com>
2020-10-14 01:35:43 +0530

    C++ code to calculate sum of n numbers

---
+++ son.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int sum=0;
+    for(int i =1;i<=n;i++)
+    {
+        sum=sum+n;
+    }
+    cout<<"Sum of "<<n<<" nos = "<<sum<<endl;
+    return 0;
+}
