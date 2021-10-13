31a7f2e9c175317815d577f6afac3dfda8bf06b7
Anjali kumari <anjali.ak6234@gmail.com>
2021-10-13 08:36:51 +0530

    Create anjaliq1.py

---
+++ Shashank Tiwari/anjaliq1.py
@@ -0,0 +1,20 @@
+print("Shashank 1928260")
+
+age=20
+print("Age is", age, ",\t","Data type is", type(age))
+age=str(age)
+print("\n")
+print("Age is", age, ",\t","Data type is",  type(age))
+
+#b) len
+string='Batman is my favourite movie'
+print(len(string))
+
+#c)
+string='I admire Bruce Wayne'
+print("Data type is",  type(string))
+
+#d)
+string="1001"
+string=int(string)
+print("Data type is", type(string))
